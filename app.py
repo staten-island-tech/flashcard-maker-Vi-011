b368a364b5d954a0e192f46ae17ff33de2b22b53
Vi-011 <vickyg11@nycstudents.net>
2025-03-25 10:17:06 -0400

    sefsef

---
+++ app.py
@@ -0,0 +1,12 @@
+class Flashcards:
+    def __init__(mode, cards, answer):
+        mode.cards = cards
+        mode.answer = answer
+    def points(cards, answer):
+
+answer = int(input("Answer?"))
+
+streak = 0
+for every answer = True:
+    streak + 1
+
