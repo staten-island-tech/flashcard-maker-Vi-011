1c94a305ccbde03f08b58c4f1e3ebd6bb12d9b1b
Vi-011 <vickyg11@nycstudents.net>
2025-03-26 10:16:13 -0400

    grdhgdh

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
+word = input("Enter answer")
+
+answer = input(f"Answer for '{word}': ").strip()
+Flashcards[word] = answer
+correct_answer = 0
+total_questions = 0
+streak = 0
+
+Flashcards = [
+    card(),
+    card(),
+    card()
+]
+
+card_data = [card.to_dict() for card in cards]
+
+with open("card.json", "w") as file:
+    json.dump(card_data, file, indent=4)
+
 class Flashcards:
     def __init__(mode, cards, answer):
         mode.cards = cards
         mode.answer = answer
     def points(cards, answer):
-
-answer = int(input("Answer?"))
-
-streak = 0
-for every answer = True:
-    streak + 1
-
+    to_dict(mode):
+    return {"make": mode.cards, mode.answer,}
+   
