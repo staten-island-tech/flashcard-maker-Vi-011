924c77df60cc57eccbaf709537e1bb15a7b7d17a
Vi-011 <vickyg11@nycstudents.net>
2025-04-09 10:17:13 -0400

    dgsfrdg

---
+++ epi.py
@@ -0,0 +1,9 @@
+
+P = 750
+N = 1
+R = 5
+D = 0
+while N < P:
+    N = N*R
+    D = D + 1 
+print (f"The number of people being infected is {N} in day {D}")
---
+++ stf.py
@@ -0,0 +1,10 @@
+
+P = 10
+N = 2
+R = 1
+D = 0
+while N < P:
+    N = (N*R) + 1 
+    D = D+1 
+print (f"The number of people being infected is {N} in day {D}")
+
